e0867f888e4b51be1581da1c09ba75d909212ba1
Jeremy Weiss <jlweiss@email.arizona.edu>
2025-11-14 16:56:11 -0700

    update data ETL functions

--- R/fxn_apiData.R
+++ R/fxn_apiData.R
@@ -16,32 +16,39 @@ fxn_apiData <- function(station, year) {
     station_id <- paste0("az", stationInfo$stn_no)
   }
   
-  apiData <- azmetr::az_hourly(
+  apiData <- azmetr::az_daily(
     station_id = station_id,
-    start_date_time = paste(year, "-01-01 00"),
-    end_date_time = paste(year, "-12-31 24")
+    start_date_time = paste(year, "-01-01"),
+    end_date_time = paste(year, "-12-31")
   ) %>% 
     dplyr::select(
       "date_datetime", 
       "date_doy",
-      "date_hour",
       "date_year", 
       "meta_needs_review", 
       "meta_station_id", 
       "meta_station_name", 
       "meta_version",
-      "precip_total", 
-      "relative_humidity", 
-      "sol_rad_total", 
-      "temp_airC", 
-      "temp_soil_10cmC", 
-      "temp_soil_50cmC", 
-      "vp_actual", 
-      "vp_deficit", 
-      "wind_spd_max_mps", 
-      "wind_spd_mps", 
-      "wind_vector_dir", 
-      "wind_vector_dir_stand_dev", 
+      "precip_total_mm",
+      "relative_humidity_max",
+      "relative_humidity_mean",
+      "relative_humidity_min",
+      "sol_rad_total",
+      "temp_air_maxC",
+      "temp_air_meanC",
+      "temp_air_minC",
+      "temp_soil_10cm_maxC",
+      "temp_soil_10cm_meanC",
+      "temp_soil_10cm_minC",
+      "temp_soil_50cm_maxC",
+      "temp_soil_50cm_meanC",
+      "temp_soil_50cm_minC",
+      "vp_actual_mean",
+      "vp_deficit_mean",
+      "wind_spd_max_mps",
+      "wind_spd_mean_mps",
+      "wind_vector_dir",
+      "wind_vector_dir_stand_dev",
       "wind_vector_magnitude"
     ) %>% 
     dplyr::mutate(date_datetime = as.character(date_datetime))
--- R/fxn_legacyData.R
+++ R/fxn_legacyData.R
@@ -10,50 +10,56 @@ fxn_legacyData <- function(station, year) {
   stationInfo <- station_list %>% 
     dplyr::filter(stn == station)
   
-  legacyData <- azmet_hourly_data_download( # from `uace-azmet/legacy-data-migration`
+  legacyData <- azmet_daily_data_download( # from `uace-azmet/legacy-data-migration`
     station_list,
     station,
     years = year
   )
   
-  legacyData <- legacyData$obs_hrly %>% 
+  legacyData <- legacyData$obs_dyly %>% 
     dplyr::select(
       "station_id",
       "station_number",
       "obs_datetime",
-      "obs_year",
       "obs_doy",
-      "obs_hour",
-      "obs_seconds",
+      "obs_year",
       "obs_version",
-      "obs_hrly_temp_air",
-      "obs_hrly_relative_humidity",
-      "obs_hrly_vpd",
-      "obs_hrly_sol_rad_total",
-      "obs_hrly_precip_total",
-      "obs_hrly_temp_soil_10cm",
-      "obs_hrly_temp_soil_50cm",
-      "obs_hrly_wind_spd",
-      "obs_hrly_wind_vector_magnitude",
-      "obs_hrly_wind_vector_dir",
-      "obs_hrly_wind_vector_dir_stand_dev",
-      "obs_hrly_wind_spd_max",
-      "obs_hrly_actual_vp"
+      "obs_dyly_precip_total",
+      "obs_dyly_relative_humidity_max",
+      "obs_dyly_relative_humidity_mean",
+      "obs_dyly_relative_humidity_min",
+      "obs_dyly_sol_rad_total",
+      "obs_dyly_temp_air_max",
+      "obs_dyly_temp_air_mean",
+      "obs_dyly_temp_air_min",
+      "obs_dyly_temp_soil_10cm_max",
+      "obs_dyly_temp_soil_10cm_mean",
+      "obs_dyly_temp_soil_10cm_min",
+      "obs_dyly_temp_soil_50cm_max",
+      "obs_dyly_temp_soil_50cm_mean",
+      "obs_dyly_temp_soil_50cm_min",
+      "obs_dyly_actual_vp_mean",
+      "obs_dyly_vpd_mean",
+      "obs_dyly_wind_spd_max",
+      "obs_dyly_wind_spd_mean",
+      "obs_dyly_wind_vector_dir",
+      "obs_dyly_wind_vector_dir_stand_dev",
+      "obs_dyly_wind_vector_magnitude"
     ) %>% 
     dplyr::mutate(
-      obs_hrly_precip_total = as.numeric(obs_hrly_precip_total),
-      obs_hrly_relative_humidity = as.numeric(obs_hrly_relative_humidity),
-      obs_hrly_sol_rad_total = as.numeric(obs_hrly_sol_rad_total),
-      obs_hrly_temp_air = as.numeric(obs_hrly_temp_air),
-      obs_hrly_temp_soil_10cm = as.numeric(obs_hrly_temp_soil_10cm),
-      obs_hrly_temp_soil_50cm = as.numeric(obs_hrly_temp_soil_50cm),
-      obs_hrly_actual_vp = as.numeric(obs_hrly_actual_vp),
-      obs_hrly_vpd = as.numeric(obs_hrly_vpd),
-      obs_hrly_wind_spd = as.numeric(obs_hrly_wind_spd),
-      obs_hrly_wind_spd_max = as.numeric(obs_hrly_wind_spd_max),
-      obs_hrly_wind_vector_dir = as.numeric(obs_hrly_wind_vector_dir),
-      obs_hrly_wind_vector_dir_stand_dev = as.numeric(obs_hrly_wind_vector_dir_stand_dev),
-      obs_hrly_wind_vector_magnitude = as.numeric(obs_hrly_wind_vector_magnitude),
+      obs_dyly_precip_total = as.numeric(obs_dyly_precip_total),
+      obs_dyly_relative_humidity = as.numeric(obs_dyly_relative_humidity),
+      obs_dyly_sol_rad_total = as.numeric(obs_dyly_sol_rad_total),
+      obs_dyly_temp_air = as.numeric(obs_dyly_temp_air),
+      obs_dyly_temp_soil_10cm = as.numeric(obs_dyly_temp_soil_10cm),
+      obs_dyly_temp_soil_50cm = as.numeric(obs_dyly_temp_soil_50cm),
+      obs_dyly_actual_vp_mean = as.numeric(obs_dyly_actual_vp_mean),
+      obs_dyly_vpd_mean = as.numeric(obs_dyly_vpd_mean),
+      obs_dyly_wind_spd = as.numeric(obs_dyly_wind_spd),
+      obs_dyly_wind_spd_max = as.numeric(obs_dyly_wind_spd_max),
+      obs_dyly_wind_vector_dir = as.numeric(obs_dyly_wind_vector_dir),
+      obs_dyly_wind_vector_dir_stand_dev = as.numeric(obs_dyly_wind_vector_dir_stand_dev),
+      obs_dyly_wind_vector_magnitude = as.numeric(obs_dyly_wind_vector_magnitude),
       date_datetime = obs_datetime
     )
   
